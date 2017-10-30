fe925bae44fc3f51ff4b32ed2f4956020c16f6e8
Gongxia Chen <chengongxia1990@gmail.com>
2017-10-29 22:01:12 -0400

    add calendar demo

--- App.js
+++ App.js
@@ -1,12 +1,35 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Calendar, CalendarList, Agenda } from 'react-native-calendars';
+
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+
+const instructions = Platform.select({
+  ios: 'This is iOS Device, \n' +
+    'Press Cmd+R to reload,\n' +
+    'Cmd+D or shake for dev menu',
+  android: 'This is Android Device, \n' + 
+    'Double tap R on your keyboard to reload,\n' +
+    'Shake or press menu button for dev menu',
+});
+
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
+          <Text style={styles.header}>
+          </Text>
+          <Calendar
+            // onDayPress={this.onDayPress}
+            style={styles.calendar}
+            hideExtraDays
+            // markedDates={{[this.state.selected]: {selected: true}}}
+          />
       </View>
     );
   }
@@ -19,4 +42,28 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  calendar: {
+    borderTopWidth: 1,
+    paddingTop: 5,
+    borderBottomWidth: 1,
+    borderColor: '#eee',
+    height: 350
+  },
+  text: {
+    textAlign: 'center',
+    borderColor: '#bbb',
+    padding: 10,
+    backgroundColor: '#eee'
+  },
+  container: {
+    flex: 1,
+    backgroundColor: 'gray'
+  },
+  header: {
+    height:50,
+    backgroundColor: "#FFF"
+  }
 });
+
+
+
