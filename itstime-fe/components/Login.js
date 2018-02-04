38aaa825fffac50633e24f6a9cf8ecb8e1aa8763
gongxia.chen <chengongxia1990@gmail.com>
2018-02-04 14:55:56 -0500

    add error message if login or singup fail

--- itstime-fe/components/Login.js
+++ itstime-fe/components/Login.js
@@ -4,7 +4,8 @@ import {
   ScrollView,
   View
 } from 'react-native';
-import { Button, FormLabel, FormInput } from 'react-native-elements';
+import { Button, FormLabel, FormInput, Text } from 'react-native-elements';
+import { loginService } from '../services/APIServices'
 
 export default class Login extends Component {
 
@@ -12,24 +13,60 @@ export default class Login extends Component {
     title: 'Login'
   })
 
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      username: null,
+      password: null,
+      errorMessage: null
+    }
+  }
+
+  async submitChange() {
+    const { username, password } = this.state;
+
+    if (!(username && password)) {
+      this.setState({ errorMessage: "all the fields must be field" })
+    } else {
+      const res = await loginService(username, password);
+
+      if (res == "login success") {
+        const { navigate } = this.props.navigation
+        // navigate('AvailableScheduleList')
+      } else if (res == "login fail") {
+        // something
+        this.setState({ errorMessage: res })
+      }
+    }
+
+  }
+
   render() {
     const { navigate } = this.props.navigation
     return (
       <View style={{ padding: 20 }}>
+        {this.state.errorMessage
+          && <Text style={styles.error}>{this.state.errorMessage}
+          </Text>}
         <ScrollView>
           <FormLabel>Username/Email</FormLabel>
-          <FormInput placeholder='Username'
-            containerStyle={{ marginBottom: 10 }} />
+          <FormInput
+            placeholder='Username'
+            containerStyle={{ marginBottom: 10 }}
+            onChangeText={(username) => this.setState({ username })} />
 
           <FormLabel>Password</FormLabel>
-          <FormInput placeholder='Password'
-            containerStyle={{ marginBottom: 10 }} />
+          <FormInput
+            placeholder='Password'
+            containerStyle={{ marginBottom: 10 }}
+            onChangeText={(password) => this.setState({ password })} />
 
           <Button
             title="Submit"
             style={styles.button}
             backgroundColor="rgb(255, 122, 0)"
-            onPress={() => navigate('AvailableScheduleList')}
+            onPress={this.submitChange.bind(this)}
           />
         </ScrollView>
 
@@ -46,11 +83,15 @@ export default class Login extends Component {
   }
 }
 
-
 const styles = StyleSheet.create({
   button: {
     marginBottom: 15,
     alignSelf: 'stretch',
     marginTop: 20
+  },
+  error: {
+    padding: 15,
+    color: 'red',
+    fontSize: 18
   }
 })
--- itstime-fe/services/APIServices.js
+++ itstime-fe/services/APIServices.js
@@ -2,7 +2,23 @@ import axios from 'axios';
 
 
 export function findAllSchedules() {
-  return axios.get("http://chengongxia.com:3000/v1/schedules.json").then(function (res) {
+  return axios.get("http://chengongxia.com:3000/v1/schedules").then(function (res) {
+    if (res.status != 200)
+      throw new Error("bad response from server" + res.status)
+    return res.data
+  })
+}
+
+export function singupService(username, password) {
+  return axios.get(`http://chengongxia.com:3000/v1/signup?username=${username}&password=${password}`).then(function (res) {
+    if (res.status != 200)
+      throw new Error("bad response from server" + res.status)
+    return res.data
+  })
+}
+
+export function loginService(username, password) {
+  return axios.get(`http://chengongxia.com:3000/v1/login?username=${username}&password=${password}`).then(function (res) {
     if (res.status != 200)
       throw new Error("bad response from server" + res.status)
     return res.data
