052ec7759357c45d685f2efcc8bc29ac9e02c8b6
gongxia.chen <chengongxia1990@gmail.com>
2018-08-30 21:01:20 -0400

    update folder name to be more clear

--- server/src/controllers/user.js
+++ server/src/controllers/user.js
@@ -7,7 +7,7 @@ export const signup = async (req, res, next) => {
   await mongoose.connect('mongodb://localhost/itstime');
   const data = req.body;
 
-  User.find({ username: data.username }, async function (err, docs) {
+  User.find({ username: data.username }, async function(err, docs) {
     if (docs.length) {
       res.status(200).send(`user ${data.username} already exist`);
     } else {
@@ -23,13 +23,13 @@ export const login = async (req, res, next) => {
   await mongoose.connect('mongodb://localhost/itstime');
   const { username, password } = req.body;
 
-  User.findOne({ username: username }).exec(function (err, user) {
+  User.findOne({ username: username }).exec(function(err, user) {
     if (err) {
       return res.status(200).send('login fail, something went wrong');
     } else if (!user) {
       return res.status(200).send(`login fail, user ${username} not exist`);
     } else {
-      bcrypt.compare(password, user.password, function (err, result) {
+      bcrypt.compare(password, user.password, function(err, result) {
         if (result === true) {
           return res.status(200).send(user._id);
         } else {
